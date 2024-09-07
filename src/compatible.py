b834d51ad78481f4da72e3b9b34f4897c0ca5553
Karenxzr <cyranvvv@hotmail.com>
2024-09-07 11:35:46 -0400

    summary

--- src/compatible.py
+++ src/compatible.py
@@ -5,7 +5,7 @@ import re
 
 
 
-def summarise_annotation(target):
+def summarise_annotation(target, gtf_path):
     def get_numeric_key(key):
         return int(key.split('_')[-1])
     file_name_final = os.path.join(target, "reference/metageneStructureInformationwNovel.pkl")
@@ -15,7 +15,7 @@ def summarise_annotation(target):
     if os.path.exists(file_name_final):
         print('novel isoform annotations exist, transforming to gtf format')
         metageneStructureInformation = load_pickle(file_name_final)
-        convert_to_gtf(metageneStructureInformation, output_file, meta=True)
+        convert_to_gtf(metageneStructureInformation, output_file, meta=True, gtf_path=gtf_path)
     elif len(file_names)>0:
         print('novel isoform annotations exist, merging and transforming to gtf format')
         metageneStructureInformation = {}
@@ -27,7 +27,7 @@ def summarise_annotation(target):
         )
         with open(file_name_final, 'wb') as file:
             pickle.dump(metageneStructureInformation, file)
-        convert_to_gtf(metageneStructureInformation, output_file, meta=True)
+        convert_to_gtf(metageneStructureInformation, output_file, meta=True, gtf_path=gtf_path)
         print('removing sub-files of annotations')
         for file_name in file_names:
             os.remove(file_name)
