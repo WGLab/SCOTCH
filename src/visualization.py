6fe331cc7a8edcfd279b03928b3aca50b029a975
Karenxzr <cyranvvv@hotmail.com>
2024-10-12 20:01:02 -0400

    vis

--- src/visualization.py
+++ src/visualization.py
@@ -186,7 +186,8 @@ def run_trackplot(target_vis, gene_name, gene_chr, gene_start, gene_end,gene_str
         "--show-junction-num",  # Show junction numbers
         "-t", str(junction_num),  # threshold
         "--intron-scale", str(intron_scale),  # Intron scaling factor
-        "--same-y"
+        "--same-y",
+        "--distance_ratio", 0.1
     ]
     try:
         subprocess.run(command, check=True)
