ba6b0bd253afffff0acf244db7186b09b04a4983
Zhuoran Xu <cyranvvv@hotmail.com>
2025-01-17 11:34:46 -0500

    Update parse_pipeline.sh

--- example/parse_pipeline.sh
+++ example/parse_pipeline.sh
@@ -12,29 +12,24 @@ source ~/.bashrc
 conda activate scats
 
 
-code_path='/home/xu3/Project_single-cell-long-read-transcriptomic-analysis/src/'
-ref='/mnt/isilon/wang_lab/karen/singlecell_parse_v10/genomes'
-output_folder='/mnt/isilon/wang_lab/karen/singlecell_parse_v10/expdata/parse_precessed'
-input_folder='/mnt/isilon/wang_lab/karen/singlecell_parse_v10/expdata/fastqgz_file'
+code_path='/path/to/onp_create_reads.py' #onp_create_reads.py is provided by Parse
+ref='/path/to/reference/genomes'
+output_folder='/path/to/output/folder'
+input_folder='/path/to/input/fastqgz_file'
 build="GRCh38"
 
-#cd $code_path
-#bash parse.sh -o $output_folder -f $input_folder \
-#	--workers 32 -r $ref --build "GRCh38"
+cd $code_path
+bash parse.sh -o $output_folder -f $input_folder --workers 32 -r $ref --build "GRCh38"
 
 
-#cd $ref
-#IDX=GRCh38.pre.mmi
-#BED=genome_reference.pre.bed
-#FQ=${output_folder}/400bps-out/process/barcode_head.fastq.gz
-#minimap2 --MD -a -u f -x splice -t 16 --junc-bed "$BED" "$IDX" "$FQ" > ${output_folder}/400bps-out/process/aligned.sam
+cd $ref
+IDX=GRCh38.pre.mmi
+BED=genome_reference.pre.bed
+FQ=${output_folder}/400bps-out/process/barcode_head.fastq.gz
+minimap2 --MD -a -u f -x splice -t 16 --junc-bed "$BED" "$IDX" "$FQ" > ${output_folder}/400bps-out/process/aligned.sam
 
 fq1=$(find "$output_folder" -name "*R1_cat.fastq.gz" -print -quit)
-#split-pipe --fq1 $fq1 --mode post --chemistry v1 \
-#    --nthreads 32 --genome_dir $ref/$build \
-#    --parfile ${code_path}/parfile.txt --output_dir ${output_folder}/400bps-out/
-
-split-pipe --fq1 $fq1 --mode mol --chemistry v1 \
+split-pipe --fq1 $fq1 --mode all --chemistry v1 \
     --nthreads 32 --genome_dir $ref/$build \
     --parfile ${code_path}/parfile.txt --output_dir ${output_folder}/400bps-out/
 
